a16686cc5fd1bb67ad14edeb983a894ce14e4be2
Ferhat Alvar <frtalvar@gmail.com>
2022-12-10 23:39:16 +0300

    created main method

--- src/Test1.java
+++ src/Test1.java
@@ -1,8 +1,8 @@
 public class Test1 {
 
 
-    public static void main(String args) {
-adfadfdfsdfsadfasdfdfsdfasdfasdfaf
+    public static void main(String [] args) {
+//adfadfdfsdfsadfasdfdfsdfasdfasdfaf
 
         //fasfdfadfasdfasd
     }
